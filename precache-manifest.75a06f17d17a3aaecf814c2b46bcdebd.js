ad885261687afbf4a72f2b0806d70668cb461524
Ily Trevino <ily.trevino@live.com>
2019-05-28 08:45:20 -0500

    Updates

---
+++ precache-manifest.75a06f17d17a3aaecf814c2b46bcdebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903b715bbd265ce661780ba29e936534",
+    "url": "/GDL002-burger-queen/burger-queen/index.html"
+  },
+  {
+    "revision": "0d871e60893bcf6438c9",
+    "url": "/GDL002-burger-queen/burger-queen/static/css/main.d9a47669.chunk.css"
+  },
+  {
+    "revision": "0e4e9d6bfd9d78b3e084",
+    "url": "/GDL002-burger-queen/burger-queen/static/js/2.c377d86a.chunk.js"
+  },
+  {
+    "revision": "0d871e60893bcf6438c9",
+    "url": "/GDL002-burger-queen/burger-queen/static/js/main.2d67d7c5.chunk.js"
+  },
+  {
+    "revision": "86bd452e5d67abdb6993",
+    "url": "/GDL002-burger-queen/burger-queen/static/js/runtime~main.fa94ad2f.js"
+  },
+  {
+    "revision": "0ec46c57113dd14cd24f574ac3f9e960",
+    "url": "/GDL002-burger-queen/burger-queen/static/media/waiter.0ec46c57.png"
+  }
+]);
